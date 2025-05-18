a3ac71a8765b4d9007b72ad08b82ed5073a3a297
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-05-18 13:41:09 +0530

    Add Routes using react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Dashboard from './pages/DashBoard';
+import TaskList from './pages/TaskList';
+import AddTask from './pages/AddTask';
+import EditTask from './pages/EditTask';
+import NotFound from './pages/NotFound';
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/tasks" element={<TaskList />} />
+      <Route path="/tasks/add" element={<AddTask />} />
+      <Route path="/tasks/edit/:id" element={<EditTask />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
-}
+};
 
 export default App;
